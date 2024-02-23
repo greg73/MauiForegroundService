032eaf653d91c4900da34ddd456d34be7fca650e
Greg Allain <Greg.Allain@datasym.co.uk>
2024-02-23 14:57:27 +0000

    Add project files.

---
+++ MauiApp2/Platforms/Android/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using MauiApp2.Platforms;
+
+namespace MauiApp2
+{
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    public class MainActivity : MauiAppCompatActivity
+    {
+        private Intent? _startServiceIntent;
+
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            _startServiceIntent = new Intent(this, typeof(TimestampService));
+            _startServiceIntent.SetAction(Constants.ACTION_START_SERVICE);
+            StartService(_startServiceIntent);
+        }
+    }
+}
---
+++ MauiApp2/Platforms/Android/Services/UtcTimestamper.cs
@@ -0,0 +1,25 @@
+﻿namespace MauiApp2.Platforms;
+
+public class UtcTimestamper
+{
+    DateTime startTime;
+    bool wasReset = false;
+
+    public UtcTimestamper()
+    {
+        startTime = DateTime.UtcNow;
+    }
+
+
+    public string GetFormattedTimestamp()
+    {
+        TimeSpan duration = DateTime.UtcNow.Subtract(startTime);
+        return wasReset ? $"Service restarted at {startTime} ({duration:c} ago)." : $"Service started at {startTime} ({duration:c} ago).";
+    }
+
+    public void Restart()
+    {
+        startTime = DateTime.UtcNow;
+        wasReset = true;
+    }
+}
